176e25d129edf282f8ab45cdab3be134947cde91
Kim Si Young <ksy4362@naver.com>
2016-01-05 19:42:54 +0900

    Organizing Files

--- Cameraman/jni/servermanager/event/EventType.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-enum class EventType
-{
-	PLUGIN_ENABLE,
-	PLUGIN_DISABLE,
-	PLAYER_JOIN,
-	PLAYER_QUIT
-};
--- Cameraman/jni/servermanager/version.h
+++
@@ -1,3 +0,0 @@
-#define VERSION_CODE 13
-#define VERSION_NAME 4.2
-#define MADE_BY "KsyMC"
